947aa5b6472cd77d137a79b1e3cef7e11b56c0ee
Paul Elisha <ireajao@yahoo.com>
2024-09-21 00:49:38 +0100

    sig update

--- src/SignUtils.sol
+++ src/SignUtils.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@permit2/interfaces/ISignatureTransfer.sol";
+import "permit2/interfaces/ISignatureTransfer.sol";
 import "forge-std/Test.sol";
 
 abstract contract SignUtils is Test {
